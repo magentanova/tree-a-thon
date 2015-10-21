244db3ecb00cc8d2373697889dc1291cea357741
pandafulmanda <pandafulmanda@gmail.com>
2015-10-20 20:52:41 -0500

    reduce trees json

---
+++ scripts/clean.js
@@ -0,0 +1,14 @@
+var _ = require('lodash');
+var treeData = require('../trees.json');
+var fs = require('fs');
+var path = require('path');
+
+var wantedColumns = ['ID Main Table', 'ID Site Name', 'Tree Planted Qty', 'Tree Species Breakdown', 'Tree Size', 'Street Number', 'Street', 'City', 'State', 'Zip']
+
+var cleanedTrees = _.map(treeData, cleaner);
+
+fs.writeFile(path.resolve(__dirname, '../trees.reduced.json'), JSON.stringify(cleanedTrees));
+
+function cleaner(tree){
+  return _.pick(tree, wantedColumns);
+}
